072cadd2e1166221651bcd7d20ceed9cb175f483
Brandon Neth <brandon.neth@hpe.com>
2025-03-31 15:29:05 -0700

    forgot to include some changes for the performance analysis
    Signed-off-by: Brandon Neth <brandon.neth@hpe.com>

--- performance/overhead.F90
+++ performance/overhead.F90
@@ -9,12 +9,16 @@ program overhead_fortran
   type(marbl_log_type)          :: driver_status_log
   character(len=char_len) :: hist_file, settings_file, hist_file_base
   character(len=3)        :: unit_system_opt
+  real :: interior(5, 60, 32)
+  real :: surface(2, 32, 5)
   integer :: num_inst
-
+  integer :: count_start, count_end, count_rate
+  real :: elapsed
   integer :: num_runs, istat, i
   character(len=10) :: arg
   character(len=10) :: run_num
-
+  real :: time_io, time_init, time_compute
+  integer ::a,b
   ! Get the first command-line argument
   call get_command_argument(1, arg)
 
@@ -27,8 +31,12 @@ program overhead_fortran
   settings_file  = 'marbl_with_o2_consumption_scalef.settings'
   num_inst = 5
 
-
+  time_io = 0
+  time_init = 0
+  time_compute = 0
   
+
+
   do i = 1,num_runs
     allocate(marbl_instances(num_inst))
     do n =1,num_inst
@@ -36,8 +44,15 @@ program overhead_fortran
     end do
     write(run_num, '(I5)') i
     hist_file = trim(run_num) // trim(hist_file_base)
-    call test(marbl_instances, hist_file, unit_system_opt, driver_status_log)
+    call test(marbl_instances, hist_file, unit_system_opt, driver_status_log, time_io, time_init, time_compute, interior, surface)
+    
     deallocate(marbl_instances)
+
   end do
 
+  print *, 'interior and surface to make the compute happen:', interior, surface
+  print '(A, F10.6)', 'IO      Time: ', time_io
+  print '(A, F10.6)', 'Init    Time: ', time_init
+  print '(A, F10.6)', 'Compute Time: ', time_compute
+  
 end program overhead_fortran
